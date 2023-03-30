0f6469decefcbb9b03824a79be0ddb925053afa1
emmanuel <gasagureemma@gmail.com>
2023-03-31 01:06:26 +0200

    first commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -14,15 +14,18 @@ const handleLogin=()=>{
    setTimeout(() => {
     setloading(false)
    },2000);
+//    setInterval(() => {
+//     console.log ({test:"Text"})
+//    }, 2000);
 
 }
 return(
 
 <>
 
-<div className="grid md:grid-cols-2 h-screen bg-gray-600">
+<div className="grid md:grid-cols-2 h-screen ">
 <div>
-<img className="w-full h-sreen md:block hidden" src="https://images.unsplash.com/photo-1661956602139-ec64991b8b16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=365&q=80"/>
+<img className="w-full h-sreen md:block hidden" src="https://images.unsplash.com/photo-1680013995061-2d6d1f3ab417?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
 </div>
 <div className="">
 <form onSubmit={handleLogin} className="p-4 max-w-lg mx-auto">
@@ -30,7 +33,7 @@ return(
     <p>Username</p>
     <input value={username}onChange={(event)=>setUsername(event.target.value)} type="text"className="p-3 rounded-lg border border-green-700 focus:outline-none w-full mb-4"/>
     <p>password</p>
-    <input value={password} onChange={(event)=>setpassword(event.target.value)} type="password"className="p-3 rounded-lg border border-green-700 focus:outline-none w-full mb-4"/>
+    <input value={password}onChange={(event)=>setpassword(event.target.value)} type="password"className="p-3 rounded-lg border border-green-700 focus:outline-none w-full mb-4"/>
     
         <p> don't you have an account?<a href=""><span className="text-green-600">signup now</span></a></p>
    {!loading&& <button type="submit" className="bg-green-600 px-4 py-2 rounded w-full my-2 ">sigin</button>}
